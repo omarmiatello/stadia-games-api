212f019d82ad797cb59d686eabaedcfebc1e3c08
Omar Miatello <jackl85@gmail.com>
2022-06-20 10:58:46 +0200

    update-api.main.kts - update API and send a Telegram notification

--- update-api.main.kts
+++ update-api.main.kts
@@ -1,21 +1,37 @@
 #!/usr/bin/env kotlin
 @file:Repository("https://repo.maven.apache.org/maven2")
-@file:DependsOn("com.github.omarmiatello.kotlin-script-toolbox:zero-setup:0.0.1")
+@file:DependsOn("com.github.omarmiatello.kotlin-script-toolbox:zero-setup:0.0.3")
+@file:DependsOn("com.github.omarmiatello.telegram:client-jvm:6.0")
+@file:DependsOn("io.ktor:ktor-client-okhttp-jvm:2.0.2")  // required for com.github.omarmiatello.telegram:client
 @file:DependsOn("org.jsoup:jsoup:1.15.1")
 
 import com.github.omarmiatello.kotlinscripttoolbox.core.launchKotlinScriptToolbox
 import com.github.omarmiatello.kotlinscripttoolbox.zerosetup.gson
+import com.github.omarmiatello.kotlinscripttoolbox.zerosetup.readJsonOrNull
+import com.github.omarmiatello.telegram.TelegramClient
 import org.jsoup.Jsoup
 import java.net.URL
+import kotlin.system.exitProcess
+
+// Models (data classes)
+data class GameListResponse(val api_version: Int, val count: Int, val games: List<Game>)
+data class Game(val title: String, val img: String, val button: String?)
 
 launchKotlinScriptToolbox(
     scriptName = "Update for Stadia Games API",
     filepathPrefix = "data/",
 ) {
     val localDebug = false
-    data class Game(val title: String, val img: String, val button: String?)
-    data class GameListResponse(val api_version: Int, val count: Int, val games: List<Game>)
 
+    // Set up: Telegram notification
+    val telegramClient = TelegramClient(apiKey = readSystemPropertyOrNull("TELEGRAM_BOT_APIKEY")!!)
+    val defaultChatId = readSystemPropertyOrNull("TELEGRAM_CHAT_ID")!!
+    suspend fun sendTelegramMessage(text: String, chatId: String = defaultChatId) {
+        println("💬 $text")
+        telegramClient.sendMessage(chat_id = chatId, text = text)
+    }
+
+    // Parse https://stadia.google.com/games
     val gamesHtml = readTextOrNull("games.html")
     val gamesDoc = if (localDebug && gamesHtml != null) {
         Jsoup.parse(gamesHtml)
@@ -23,7 +39,6 @@ launchKotlinScriptToolbox(
         Jsoup.parse(URL("https://stadia.google.com/games"), 10000)
             .also { if (localDebug) writeText("games.html", it.toString()) }
     }
-
     val games = gamesDoc.select(".tLwy5")
         .map { gameDoc ->
             Game(
@@ -37,11 +52,26 @@ launchKotlinScriptToolbox(
         .distinctBy { it.title }
         .sortedBy { it.title }
 
-    writeText("games.json", gson.toJson(
-        GameListResponse(
-            api_version = 1,
-            count = games.size,
-            games = games,
-        )
-    ))
+    // Prepare for comparison of GameListResponse
+    val oldResponse = readJsonOrNull<GameListResponse>("games.json")
+    val newResponse = GameListResponse(api_version = 1, count = games.size, games = games)
+
+    // Update API: data/games.json
+    writeText("games.json", gson.toJson(newResponse))
+
+    // Send notification
+    if (oldResponse != null) {
+        val oldTitles = oldResponse.games.map { it.title }.toSet()
+        val newGames = newResponse.games.filter { it.title !in oldTitles }
+        if (newGames.isNotEmpty()) {
+            val gamesString = newGames.joinToString { it.title }
+            sendTelegramMessage(if (newGames.size == 1) {
+                "There is a new game: $gamesString"
+            } else {
+                "There are ${newGames.size} new games: $gamesString"
+            })
+        }
+    }
 }
+
+exitProcess(status = 0)
